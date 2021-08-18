581255718f95176ba7a9a99da492893e57235078
ParkKyeongHwan <youcantalktome@naver.com>
2021-08-19 00:20:00 +0900

    Add EventEnum TestCases

---
+++ UnitTest/EventEnumTests.cs
@@ -0,0 +1,82 @@
+﻿using GenericLibrary.Structures;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using UnitTest.TestHelper;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class EventEnumTests
+    {
+        [Test]
+        public void Current_ShouldThrowException_WhenImmediatelyAfterConstruction()
+        {
+            EventEnum eventEnum = new EventEnum(new Event[2]
+            {
+                new Event("Test1", "Test1".GetHash()),
+                new Event("Test2", "Test2".GetHash())
+            });
+
+            Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                // dummy is not used.
+                var dummy = eventEnum.Current;
+            });
+        }
+
+        [Test]
+        public void MoveNext_ShouldReturnTrue_IfCurrentPositionIsLessThanArray()
+        {
+            EventEnum eventEnum = new EventEnum(new Event[2]
+            {
+                new Event("Test1", "Test1".GetHash()),
+                new Event("Test2", "Test2".GetHash())
+            });
+
+            Assert.IsTrue(eventEnum.MoveNext());
+        }
+
+        [Test]
+        public void MoveNext_ShouldReturnFalse_IfCurrentPositionIsGreatherThanArray()
+        {
+            EventEnum eventEnum = new EventEnum(new Event[1]
+            {
+                new Event("Test1", "Test1".GetHash())
+            });
+
+            eventEnum.MoveNext();
+            
+            // Act and Assertion
+            Assert.IsFalse(eventEnum.MoveNext());
+        }
+
+        [Test]
+        public void Reset_ShouldBeInitializedPosition_WhenThisMethodIsCalled()
+        {
+            // initialized position is -1
+            int position = -1;
+
+            EventEnum eventEnum = new EventEnum(new Event[3]
+            {
+                new Event("Test1", "Test1".GetHash()),
+                new Event("Test2", "Test2".GetHash()),
+                new Event("Test3", "Test3".GetHash())
+            });
+
+            // change position to 0.
+            eventEnum.MoveNext();
+
+            position = eventEnum.GetFieldValue<int>("position");
+
+            Warn.If(position == -1);
+
+            // Act.
+            eventEnum.Reset();
+
+            position = eventEnum.GetFieldValue<int>("position");
+
+            Assert.AreEqual(-1, position);
+        }
+    }
+}
---
+++ UnitTest/TestHelper/ReflectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace UnitTest.TestHelper
+{
+    public static class ReflectionExtensions
+    {
+        internal static T GetFieldValue<T>(this object obj, string name)
+        {
+            // Set the flags so that private and public fields from instances will be found
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+            var field = obj.GetType().GetField(name, bindingFlags);
+
+            return (T)field?.GetValue(obj);
+        }
+    }
+}
